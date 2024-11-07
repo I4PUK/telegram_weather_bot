37f3041ee5a256995f2872b7ac4947657c45984f
mishukoffs <Mis.Serj@yandex.ru>
2024-11-07 20:44:28 +0300

    Rename test

--- main_test.go
+++ main_test.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"os"
 	"testing"
 )
 
-func TestBotToken(t *testing.T) {
+func TestReadEnvFile(t *testing.T) {
 	configErr := godotenv.Load("configs/config.env")
 
 	if configErr != nil {
 		t.Fatalf("Error loading .env file")
-	}
-
-	telegramBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
-
-	if telegramBotToken == "" {
-		t.Fatalf("Token variable not set")
 	}
 }
